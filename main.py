58b546d5fce78913896849f8a7217f156015f532
chiyanz <jonathanqyz@gmail.com>
2023-12-08 21:57:58 -0500

    Udpate to data cleaning and storage mechanism

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
+# json parsing and csv data tools
+import pandas as pd
 import json
 
 # ignore unneccesary SSL warnings 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# driver = webdriver.Chrome()
+
 
 # init driver
 driver = webdriver.Chrome( 
     options=options
 )
 
+# read config file for 
 f= open("site_data.json")
 config = json.load(f)
 
 urls = config['urls']
+studios = {studio: [] for studio, _ in urls.items()}
 
 for studio_name, url in urls.items():
   print(studio_name, url)
@@ -34,5 +40,10 @@ for studio_name, url in urls.items():
         # relocate all date buttons after page refresh
         classes = driver.find_elements(By.XPATH, "//div[@class='bw-session']")
         for session in classes:
-          print('This is a separate class: ' + session.text + "\n")
-
+          info = session.text.split('\n')
+          # trim off "view details"
+          if info[-1] == 'View details':
+             info = info[:-1]
+          studios[studio_name].append(info)
+          print(info)
+print(studios['Peri'])
