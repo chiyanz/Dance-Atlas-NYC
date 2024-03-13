f0329446acd48ad79e4de7b4772b9d0bf119071b
chiyanz <jonathanqyz@gmail.com>
2024-03-12 20:58:28 -0400

    update to include Modega crawler and github action

--- studio_crawler.py
+++ studio_crawler.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 # json parsing and csv data tools
 import pandas as pd
 import json
+import re
 
 
 class StudioCrawler: 
@@ -30,7 +31,7 @@ class StudioCrawler:
     """
     def __init__(self, studios: {str: str} = None) -> None:
         options = webdriver.ChromeOptions()
-        # options.add_argument("--headless")
+        # options.add_argument("--headless") # for production: disable browser display 
         options.add_argument("--ignore-certificate-error")
         options.add_argument("--ignore-ssl-errors")
         user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
@@ -44,11 +45,13 @@ class StudioCrawler:
       for studio_name, url in self.studios.items():
         print(studio_name, url)
         self.driver.get(url)
-
+        self.driver.implicitly_wait(5)
         if studio_name == 'Peri':
            self.peri_handler()
-        # if studio_name == 'PMT':
-        #    self.pmt_handler()
+        if studio_name == 'PMT':
+           self.pmt_handler()
+        if studio_name == 'Modega':
+           self.modega_handler()
     
     def find(self, selector):
       # finds an element by its xpath selector
@@ -60,9 +63,51 @@ class StudioCrawler:
 
 
     def peri_handler(self):
+        dates = []
+        try:
+          wait = WebDriverWait(self.driver, 10)
+          dates = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//td[contains(@class, 'bw-calendar__day') and not(contains(@class, 'bw-calendar__day--past'))]")))
+        except Exception as e:
+          print(e)
+        # dates = self.driver.find_elements(By.XPATH, "//td[contains(@class, 'bw-calendar__day') and not(contains(@class, 'bw-calendar__day--past'))]")
+        # workaround for StaleElementReferenceException: relocate all dates and use a counter instead of iterate through them
+        available_dates = len(dates)
+
+        for i in range(available_dates):
+            print(f"attempt to click day {i} button")
+            self.driver.implicitly_wait(10)
+            try:
+              dates[i].click()
+            except:
+              dates = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//td[contains(@class, 'bw-calendar__day') and not(contains(@class, 'bw-calendar__day--past'))]")))
+              dates[i].click()
+            print(f"day {i} clicked")
+
+            try:
+              classes = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='bw-session']")))
+              for session in classes:
+                print(session.text)
+                start_time = session.find_element(By.XPATH, ".//time[@class='hc_starttime']").get_attribute('datetime')
+                end_time = session.find_element(By.XPATH, ".//time[@class='hc_endtime']").get_attribute('datetime')
+                session_name = session.find_element(By.XPATH, ".//div[@class='bw-session__name']").text
+                instructor = session.find_element(By.XPATH, ".//div[@class='bw-session__staff']").text
+                info = {
+                  'start_time': start_time,
+                  'end_time': end_time,
+                  'session_name': session_name,
+                  'instructor': instructor
+                }
+                print(info)
+                self.data['Peri'].append(info) #TODO: future storage in csv or database
+            except Exception as e:
+               print(f'no classes found on day {i}')
+      
+    # on-hold due to JS injection 
+    def pmt_handler(self):
       # self.driver.implicitly_wait(3)
         wait = WebDriverWait(self.driver, 10)
-
+        close_button = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[contains(@class, 'wixui-lightbox__close-button')]")))
+        close_button.click()
         dates = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//td[contains(@class, 'bw-calendar__day') and not(contains(@class, 'bw-calendar__day--past'))]")))
         # dates = self.driver.find_elements(By.XPATH, "//td[contains(@class, 'bw-calendar__day') and not(contains(@class, 'bw-calendar__day--past'))]")
         # workaround for StaleElementReferenceException: relocate all dates and use a counter instead of iterate through them
@@ -104,48 +149,51 @@ class StudioCrawler:
               # print(f'no classes found on day {i}')
                print('encountered exception', e)
 
-      
-    
-    def pmt_handler(self):
-      self.driver.implicitly_wait(3)
-      try: 
-        dates = self.driver.find_elements(By.XPATH, "//td[contains(@class, 'bw-calendar__day') and not(contains(@class, 'bw-calendar__day--past'))]")
-        print(dates)
-        # workaround for StaleElementReferenceException: relocate all dates and use a counter instead of iterate through them
-        available_dates = len(dates)
-        for i in range(1, available_dates):
-            # relocate all the date buttons to prevent staleelement exception
-            dates = self.driver.find_elements(By.XPATH, "//td[contains(@class, 'bw-calendar__day') and not(contains(@class, 'bw-calendar__day--past'))]")
+    def modega_handler(self):
+      wait = WebDriverWait(self.driver, 10)
+      dates = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//*[contains(@class, 'd-flex') and contains(@class, 'flex-column') and contains(@class, 'week-range__day') and not(contains(@class, 'week-range--disabled'))]")))
+      available_dates = len(dates)
+
+      for i in range(available_dates):
+          print(f"attempt to click day {i} button")
+          self.driver.implicitly_wait(10)
+          try:
             dates[i].click()
-            try:
-                # Wait up to 10 seconds for the page to be loaded
-                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'html')))
-                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'bw-widget__sessions')))
-            except:
-                print("Page did not load in 10 seconds!")
-            
-            # try:
-            #   self.driver.find_elements(By.XPATH, "//div[@class='bw-session']")
-            # except:
-            #    print("Some error occured while finding classes")
-            classes = [session.text for session in self.driver.find_elements(By.XPATH, "//div[@class='bw-session__info']")]
+          except:
+            dates = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//td[contains(@class, 'bw-calendar__day') and not(contains(@class, 'bw-calendar__day--past'))]")))
+            dates[i].click()
+          print(f"day {i} clicked")
+          # reloate dates
+          #     WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+          #     WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'bw-widget__sessions')))
+          # except:
+          #     print("Page did not load in 10 seconds!")
+
+          
+          try:
+            classes = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//div[contains(concat(' ', normalize-space(@class), ' '), ' p-1 ') and contains(concat(' ', normalize-space(@class), ' '), ' card-body ')]")))
+            print(len(classes))
             for session in classes:
-              print('1 class')
-              start_time = session.find_element(By.XPATH, "//time[@class='hc_starttime']").get_attribute('datetime')
-              end_time = session.find_element(By.XPATH, "//time[@class='hc_endtime']").get_attribute('datetime')
-              session_name = session.find_element(By.XPATH, "//div[@class='bw-session__name']").text
-              instructor = session.find_element(By.XPATH, "//div[@class='bw-session__staff']").text
+              start_time = session.find_element(By.XPATH, ".//p[contains(@class, 'dateTimeText') and contains(@class, 'card-text')]").text
+              duration = re.match(r"\((\d+\smin)\)", start_time)
+              session_name = session.find_element(By.XPATH, ".//div[contains(@class, 'card-title')]").text
+              misc_info = session.find_elements(By.XPATH, ".//p[contains(@class, 'card-text')]")
+              instructor = misc_info[1].text
+              location = misc_info[2].text
+              print(instructor)
               info = {
                 'start_time': start_time,
-                'end_time': end_time,
+                'duration': duration,
                 'session_name': session_name,
-                'instructor': instructor
+                'instructor': instructor,
+                'location': location
               }
               print(info)
-              self.data['PMT'].append(info) #TODO: future storage in csv or database
-      except Exception as e:
-         print(e)
-
+              self.data['Modega'].append(info) #TODO: future storage in csv or database
+          except Exception as e:
+              print(f'no classes found on day {i}')
+              print(e)
+      return
 
 
 if __name__ == "__main__":
@@ -156,5 +204,7 @@ if __name__ == "__main__":
   crawler = StudioCrawler(studio_urls)
   crawler.crawlSessions()
   studios = {studio: [] for studio, _ in studio_urls.items()}
+  with open('crawler_results.json', 'w', encoding='utf-8') as f:
+    json.dump(studios, f, ensure_ascii=False, indent=4)
   print(crawler.data)
 
