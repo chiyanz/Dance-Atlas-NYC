d1a9988014aaeb81f3f81fd057a203627c7af557
chiyanz <jonathanqyz@gmail.com>
2024-06-16 23:33:11 -0700

    dependency update

--- database/studio_crawler.py
+++ database/studio_crawler.py
@@ -37,8 +37,9 @@ class StudioCrawler:
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')  
         options.add_argument('--disable-dev-shm-usage')        
-        options.add_argument("--ignore-certificate-error")
+        options.add_argument("--ignore-certificate-errors")
         options.add_argument("--ignore-ssl-errors")
+        options.add_argument("--ssl-protocol=any")
         user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
         options.add_argument(f'user-agent={user_agent}')
         self.driver = webdriver.Chrome(options=options)
